c2f8b173f5c24e2dd7d2148d4fec6a6b5de0acc5
Duy K. Bui <duy@buifamily.info>
2020-10-28 01:45:32 -0400

    feat: Add AppState

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,93 @@
+import { createEffect } from 'solid-js';
+
+import AppState from './AppState';
 import './App.css';
 
+const Job = ({ base, job, title }) => {
+  const assign = (event) => base.assign(job);
+  const unassign = (event) => base.unassign(job);
+  const jobSignal = base[job].bind(base);
+  const maxKey = `${job}Max`;
+  const maxSignal = base[maxKey].bind(base);
+  return (
+    <div>
+      {title} <code>{jobSignal()}</code>/<code>{maxSignal()}</code>
+      <Show when={jobSignal() < maxSignal() && base.worker() > 0}>
+        <button onClick={assign}>Assign</button>
+      </Show>
+      <Show when={jobSignal() > 0}>
+        <button onClick={unassign}>Unassign</button>
+      </Show>
+    </div>
+  );
+};
+
+const Building = ({ base, building, title }) => {
+  const build = (event) => base.build(building);
+  return (
+    <div>
+      {title} <code>{base[building]()}</code> <pre>{JSON.stringify(base.buildCost(building))}</pre>
+      <Show when={base.buildValid(building)}>
+        <button onClick={build}>Build</button>
+      </Show>
+    </div>
+  );
+};
+
+const Base = ({ base }) => {
+  return (
+    <>
+      <div>Base</div>
+      <section>
+        <Building base={base} building='house' title='House'></Building>
+        <Building base={base} building='farm' title='Farm'></Building>
+      </section>
+      <section>
+        <div>
+          Worker <code>{base.worker()}</code>
+        </div>
+        <Job base={base} job='farmer' title='Farmer'></Job>
+        <Job base={base} job='logger' title='Logger'></Job>
+        <Job base={base} job='quarrier' title='Quarrier'></Job>
+      </section>
+    </>
+  );
+};
+
 function App() {
-  return <></>;
+  const state = new AppState();
+  createEffect(state.effect.bind(state));
+
+  const endDay = state.endDay.bind(state);
+  const reset = state.reset.bind(state);
+
+  return (
+    <>
+      <section>
+        <div>
+          Day <code>{state.day()}</code>
+        </div>
+        <div>
+          Population <code>{state.population()}</code>
+        </div>
+        <button onClick={endDay}>End Day</button>
+        <button onClick={reset}>Reset</button>
+      </section>
+      <section>
+        <div>
+          Food <code>{state.food()}</code>/<code>{state.foodMax()}</code> (+<code>{state.foodRate()}</code> from workers, -<code>{state.eatRate()}</code> from
+          population)
+        </div>
+        <div>
+          Wood <code>{state.wood()}</code>/<code>{state.woodMax()}</code> (+<code>{state.woodRate()}</code> from workers)
+        </div>
+        <div>
+          Stone <code>{state.stone()}</code>/<code>{state.stoneMax()}</code> (+<code>{state.stoneRate()}</code> from workers)
+        </div>
+      </section>
+      <Base base={state.baseState(0)}></Base>
+    </>
+  );
 }
 
 export default App;
---
+++ src/AppState.test.js
@@ -0,0 +1,231 @@
+import AppState from './AppState';
+
+let state, base0;
+
+beforeEach(() => {
+  state = new AppState();
+  base0 = state.baseState(0);
+});
+
+it('resets to day 0 on-demand', () => {
+  state.setState({ day: 10 });
+  base0.setState({ house: 10 });
+  state.reset();
+  expect(state.day()).toBe(0);
+  expect(base0.house()).toBe(1);
+});
+
+it('starts at Day 0 and increases everyday', () => {
+  expect(state.day()).toBe(0);
+  state.endDay();
+  expect(state.day()).toBe(1);
+});
+
+it('starts with first base', () => {
+  expect(state.bases()[0].active).toBe(true);
+});
+
+it('has a house in first base', () => {
+  expect(base0.house()).toBe(1);
+});
+
+it('calculates populationMax = house * 2', () => {
+  expect(base0.populationMax()).toBe(base0.house() * 2);
+});
+
+it('has a worker in first base', () => {
+  expect(base0.worker()).toBe(1);
+});
+
+it('adds a worker everyday if population < populationMax and food > 0', () => {
+  const worker1 = base0.worker();
+  state.setState({ food: 0 });
+  state.endDay();
+  expect(base0.worker()).toBe(worker1);
+
+  state.setState({ food: 3000 });
+  state.endDay();
+  const worker2 = base0.worker();
+  expect(worker2).toBe(worker1 + 1);
+
+  state.endDay();
+  const worker3 = base0.worker();
+  expect(worker3).toBe(worker2);
+});
+
+it('calculates population = workers', () => {
+  expect(base0.population()).toBe(base0.worker() + base0.farmer() + base0.logger() + base0.quarrier());
+});
+
+it('starts with 3000 food', () => {
+  expect(state.food()).toBe(3000);
+});
+
+it('starts with 3000 foodMax', () => {
+  expect(state.foodMax()).toBe(3000);
+});
+
+it('eats 1 food everyday per population', () => {
+  expect(state.eatRate()).toBe(state.population());
+});
+
+it('supports 30 loggers in first base', () => {
+  expect(base0.loggerMax()).toBe(30);
+});
+
+it('assigns logger if worker > 0 && logger < loggerMax', () => {
+  base0.setState({ worker: 1, logger: 0, loggerMax: 30 });
+  base0.assign('logger');
+  expect(base0.logger()).toBe(1);
+
+  base0.setState({ worker: 0, logger: 0, loggerMax: 30 });
+  base0.assign('logger');
+  expect(base0.logger()).toBe(0);
+
+  base0.setState({ worker: 1, logger: 0, loggerMax: 0 });
+  base0.assign('logger');
+  expect(base0.logger()).toBe(0);
+});
+
+it('unassigns logger if logger > 0', () => {
+  base0.setState({ logger: 0 });
+  base0.unassign('logger');
+  expect(base0.logger()).toBe(0);
+
+  base0.setState({ logger: 1 });
+  base0.unassign('logger');
+  expect(base0.logger()).toBe(0);
+});
+
+it('starts with no wood', () => {
+  expect(state.wood()).toBe(0);
+});
+
+it('starts with 1500 woodMax', () => {
+  expect(state.woodMax()).toBe(1500);
+});
+
+it('generates 5 wood per logger', () => {
+  base0.assign('logger');
+  expect(state.woodRate()).toBe(state.logger() * 5);
+});
+
+it('adds wood everyday until reaching foodMax', () => {
+  base0.assign('logger');
+  const wood = state.wood();
+  const woodRate = state.woodRate();
+  const woodMax = state.woodMax();
+  state.endDay();
+  expect(state.wood()).toBe(Math.min(woodMax, wood + woodRate));
+});
+
+it('starts with no farm in first base', () => {
+  expect(base0.farm()).toBe(0);
+});
+
+it('calculates farmerMax = farm * 2', () => {
+  expect(base0.farmerMax()).toBe(base0.farm() * 2);
+});
+
+it('calculates costs from existing buildings', () => {
+  const { wood } = base0.buildCost('farm');
+  base0.setState({ farm: 10 });
+  expect(base0.buildCost('farm').wood).toBeGreaterThan(wood);
+});
+
+it('calculates affordability from costs', () => {
+  const { wood } = base0.buildCost('farm');
+  expect(base0.buildValid('farm')).toBeFalsy();
+  state.setState({ wood });
+  expect(base0.buildValid('farm')).toBeTruthy();
+});
+
+it('builds a farm if enough wood', () => {
+  const farm = base0.farm();
+  const { wood } = base0.buildCost('farm');
+  state.setState({ wood });
+  base0.build('farm');
+  expect(base0.farm()).toBe(farm + 1);
+  expect(state.wood()).toBeLessThan(wood);
+});
+
+it('assigns farmer if worker > 0 && farmer < farmerMax', () => {
+  base0.setState({ worker: 1, farmer: 0, farm: 1 });
+  base0.assign('farmer');
+  expect(base0.farmer()).toBe(1);
+
+  base0.setState({ worker: 0, farmer: 0, farm: 1 });
+  base0.assign('farmer');
+  expect(base0.farmer()).toBe(0);
+
+  base0.setState({ worker: 1, farmer: 0, farm: 0 });
+  base0.assign('farmer');
+  expect(base0.farmer()).toBe(0);
+});
+
+it('generates 4 food per farmer', () => {
+  base0.setState({ worker: 1, farmer: 0, farm: 1 });
+  base0.assign('farmer');
+  expect(state.foodRate()).toBe(state.farmer() * 4);
+});
+
+it('adds food everyday until reaching foodMax', () => {
+  base0.setState({ worker: 1, farmer: 0, farm: 1 });
+  base0.assign('farmer');
+  const food = state.food();
+  const foodRate = state.foodRate();
+  const population = state.population();
+  const foodMax = state.foodMax();
+  state.endDay();
+  expect(state.food()).toBe(Math.min(foodMax, food + foodRate - population));
+});
+
+it('starts with no stone', () => {
+  expect(state.stone()).toBe(0);
+});
+
+it('starts with same stoneMax as woodMax', () => {
+  expect(state.stoneMax()).toBe(state.woodMax());
+});
+
+it('supports 30 quarriers in first base', () => {
+  expect(base0.quarrierMax()).toBe(30);
+});
+
+it('assigns quarrier if worker > 0 && quarrier < quarrierMax', () => {
+  base0.setState({ worker: 1, quarrier: 0, quarrierMax: 30 });
+  base0.assign('quarrier');
+  expect(base0.quarrier()).toBe(1);
+
+  base0.setState({ worker: 0, quarrier: 0, quarrierMax: 30 });
+  base0.assign('quarrier');
+  expect(base0.quarrier()).toBe(0);
+
+  base0.setState({ worker: 1, quarrier: 0, quarrierMax: 0 });
+  base0.assign('quarrier');
+  expect(base0.quarrier()).toBe(0);
+});
+
+it('generates 3 stone per quarrier', () => {
+  base0.assign('quarrier');
+  expect(state.stoneRate()).toBe(state.quarrier() * 3);
+});
+
+it('adds stone everyday until reaching stoneMax', () => {
+  base0.assign('quarrier');
+  const stone = state.stone();
+  const stoneRate = state.stoneRate();
+  const stoneMax = state.stoneMax();
+  state.endDay();
+  expect(state.stone()).toBe(Math.min(stoneMax, stone + stoneRate));
+});
+
+it('builds a house if enough wood and stone', () => {
+  const house = base0.house();
+  const { wood, stone } = base0.buildCost('house');
+  state.setState({ wood, stone });
+  base0.build('house');
+  expect(base0.house()).toBe(house + 1);
+  expect(state.wood()).toBeLessThan(wood);
+  expect(state.stone()).toBeLessThan(stone);
+});
