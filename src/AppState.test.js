c1cbee1dc1375262f6f576550c3ea38dd333ffab
Duy K. Bui <duy@buifamily.info>
2020-10-31 21:18:20 -0400

    refactor: Make building / job effects configurable

--- src/AppState.test.js
+++ src/AppState.test.js
@@ -1,36 +1,40 @@
 import AppState, { QuestState } from './AppState';
 
-let state, base0;
+let app, base0;
 
 beforeEach(() => {
-  state = new AppState();
-  base0 = state.baseState(0);
+  app = new AppState();
+  base0 = app.baseState(0);
 });
 
 it('resets to day 0 on-demand', () => {
-  state.setState({ day: 10 });
+  app.setState({ day: 10 });
   base0.setState({ house: 10 });
-  state.reset();
-  expect(state.day()).toBe(0);
+  app.reset();
+  expect(app.day()).toBe(0);
   expect(base0.house()).toBe(1);
 });
 
 it('starts at Day 0 and increases everyday', () => {
-  expect(state.day()).toBe(0);
-  state.endDay();
-  expect(state.day()).toBe(1);
+  expect(app.day()).toBe(0);
+  app.endDay();
+  expect(app.day()).toBe(1);
 });
 
 it('starts with first base', () => {
-  expect(state.bases().length).toBe(1);
+  expect(app.bases().length).toBe(1);
 });
 
 it('has a house in first base', () => {
   expect(base0.house()).toBe(1);
 });
 
-it('calculates populationMax = house * 2', () => {
-  expect(base0.populationMax()).toBe(base0.house() * 2);
+it('starts with houseEffect = 2', () => {
+  expect(app.houseEffect()).toBe(2);
+});
+
+it('calculates populationMax = house * houseEffect', () => {
+  expect(base0.populationMax()).toBe(base0.house() * app.houseEffect());
 });
 
 it('has a worker in first base', () => {
@@ -39,16 +43,16 @@ it('has a worker in first base', () => {
 
 it('adds a worker everyday if population < populationMax and food > 0', () => {
   const worker1 = base0.worker();
-  state.setState({ food: 0 });
-  state.endDay();
+  app.setState({ food: 0 });
+  app.endDay();
   expect(base0.worker()).toBe(worker1);
 
-  state.setState({ food: 3000 });
-  state.endDay();
+  app.setState({ food: 3000 });
+  app.endDay();
   const worker2 = base0.worker();
   expect(worker2).toBe(worker1 + 1);
 
-  state.endDay();
+  app.endDay();
   const worker3 = base0.worker();
   expect(worker3).toBe(worker2);
 });
@@ -58,15 +62,15 @@ it('calculates population = workers', () => {
 });
 
 it('starts with 3000 food', () => {
-  expect(state.food()).toBe(3000);
+  expect(app.food()).toBe(3000);
 });
 
 it('starts with 3000 foodMax', () => {
-  expect(state.foodMax()).toBe(3000);
+  expect(app.foodMax()).toBe(3000);
 });
 
 it('consumes 1 food per population', () => {
-  expect(state.foodConsumeRate()).toBe(state.population());
+  expect(app.foodConsumeRate()).toBe(app.population());
 });
 
 it('supports 30 loggers in first base', () => {
@@ -98,33 +102,41 @@ it('unassigns logger if logger > 0', () => {
 });
 
 it('starts with no wood', () => {
-  expect(state.wood()).toBe(0);
+  expect(app.wood()).toBe(0);
 });
 
 it('starts with 1500 woodMax', () => {
-  expect(state.woodMax()).toBe(1500);
+  expect(app.woodMax()).toBe(1500);
 });
 
-it('produces 5 wood per logger', () => {
+it('starts with loggerEffect = 5', () => {
+  expect(app.loggerEffect()).toBe(5);
+});
+
+it('produces wood = logger * loggerEffect ', () => {
   base0.assign('logger');
-  expect(state.woodProduceRate()).toBe(state.logger() * 5);
+  expect(app.woodProduceRate()).toBe(app.logger() * app.loggerEffect());
 });
 
 it('adds wood everyday until reaching woodMax', () => {
   base0.assign('logger');
-  const wood = state.wood();
-  const woodRate = state.woodRate();
-  const woodMax = state.woodMax();
-  state.endDay();
-  expect(state.wood()).toBe(Math.min(woodMax, wood + woodRate));
+  const wood = app.wood();
+  const woodRate = app.woodRate();
+  const woodMax = app.woodMax();
+  app.endDay();
+  expect(app.wood()).toBe(Math.min(woodMax, wood + woodRate));
 });
 
 it('starts with no farm in first base', () => {
   expect(base0.farm()).toBe(0);
 });
 
-it('calculates farmerMax = farm * 2', () => {
-  expect(base0.farmerMax()).toBe(base0.farm() * 2);
+it('starts with same farmEffect as houseEffect', () => {
+  expect(app.farmEffect()).toBe(app.houseEffect());
+});
+
+it('calculates farmerMax = farm * farmEffect', () => {
+  expect(base0.farmerMax()).toBe(base0.farm() * app.farmEffect());
 });
 
 it('calculates costs from existing buildings', () => {
@@ -136,17 +148,17 @@ it('calculates costs from existing buildings', () => {
 it('calculates affordability from costs', () => {
   const { wood } = base0.buildCost('farm');
   expect(base0.buildValid('farm')).toBeFalsy();
-  state.setState({ wood });
+  app.setState({ wood });
   expect(base0.buildValid('farm')).toBeTruthy();
 });
 
 it('builds a farm if enough wood', () => {
   const farm = base0.farm();
   const { wood } = base0.buildCost('farm');
-  state.setState({ wood });
+  app.setState({ wood });
   base0.build('farm');
   expect(base0.farm()).toBe(farm + 1);
-  expect(state.wood()).toBeLessThan(wood);
+  expect(app.wood()).toBeLessThan(wood);
 });
 
 it('assigns farmer if worker > 0 && farmer < farmerMax', () => {
@@ -163,28 +175,28 @@ it('assigns farmer if worker > 0 && farmer < farmerMax', () => {
   expect(base0.farmer()).toBe(0);
 });
 
-it('produces 4 food per farmer', () => {
+it('produces food = farmer * farmerEffect', () => {
   base0.setState({ worker: 1, farmer: 0, farm: 1 });
   base0.assign('farmer');
-  expect(state.foodProduceRate()).toBe(state.farmer() * 4);
+  expect(app.foodProduceRate()).toBe(app.farmer() * app.farmerEffect());
 });
 
 it('adds food everyday until reaching foodMax', () => {
   base0.setState({ worker: 1, farmer: 0, farm: 1 });
   base0.assign('farmer');
-  const food = state.food();
-  const foodRate = state.foodProduceRate();
-  const foodMax = state.foodMax();
-  state.endDay();
-  expect(state.food()).toBe(Math.min(foodMax, food + foodRate));
+  const food = app.food();
+  const foodRate = app.foodProduceRate();
+  const foodMax = app.foodMax();
+  app.endDay();
+  expect(app.food()).toBe(Math.min(foodMax, food + foodRate));
 });
 
 it('starts with no stone', () => {
-  expect(state.stone()).toBe(0);
+  expect(app.stone()).toBe(0);
 });
 
 it('starts with same stoneMax as woodMax', () => {
-  expect(state.stoneMax()).toBe(state.woodMax());
+  expect(app.stoneMax()).toBe(app.woodMax());
 });
 
 it('supports 30 quarriers in first base', () => {
@@ -205,18 +217,18 @@ it('assigns quarrier if worker > 0 && quarrier < quarrierMax', () => {
   expect(base0.quarrier()).toBe(0);
 });
 
-it('produces 3 stone per quarrier', () => {
+it('produces stone = quarrier * quarrierEffect', () => {
   base0.assign('quarrier');
-  expect(state.stoneProduceRate()).toBe(state.quarrier() * 3);
+  expect(app.stoneProduceRate()).toBe(app.quarrier() * app.quarrierEffect());
 });
 
 it('adds stone everyday until reaching stoneMax', () => {
   base0.assign('quarrier');
-  const stone = state.stone();
-  const stoneRate = state.stoneRate();
-  const stoneMax = state.stoneMax();
-  state.endDay();
-  expect(state.stone()).toBe(Math.min(stoneMax, stone + stoneRate));
+  const stone = app.stone();
+  const stoneRate = app.stoneRate();
+  const stoneMax = app.stoneMax();
+  app.endDay();
+  expect(app.stone()).toBe(Math.min(stoneMax, stone + stoneRate));
 });
 
 it('builds a house if enough wood and stone', () => {
@@ -224,32 +236,32 @@ it('builds a house if enough wood and stone', () => {
   expect(base0.build('house')).toBe(false);
 
   const { wood, stone } = base0.buildCost('house');
-  state.setState({ wood, stone });
+  app.setState({ wood, stone });
   expect(base0.build('house')).toBe(true);
   expect(base0.house()).toBe(house + 1);
-  expect(state.wood()).toBeLessThan(wood);
-  expect(state.stone()).toBeLessThan(stone);
+  expect(app.wood()).toBeLessThan(wood);
+  expect(app.stone()).toBeLessThan(stone);
 });
 
 it('passes a quest condition', () => {
-  const quest1 = new QuestState(state, 'test', 'true', ['true'], ['true']);
+  const quest1 = new QuestState(app, 'test', 'true', ['true'], ['true']);
   expect(quest1.complete()).toBe(true);
 
-  const quest2 = new QuestState(state, 'test', 'false', ['false'], ['false']);
+  const quest2 = new QuestState(app, 'test', 'false', ['false'], ['false']);
   expect(quest2.complete()).toBe(false);
 
-  const quest3 = new QuestState(state, 'test', 'farm', ['base0.farm > 0'], ['this.app.baseState(0).farm() > 0']);
+  const quest3 = new QuestState(app, 'test', 'farm', ['base0.farm > 0'], ['this.app.baseState(0).farm() > 0']);
   expect(quest3.complete()).toBe(false);
   base0.setState({ farm: 1 });
   expect(quest3.complete()).toBe(true);
 });
 
 it('lists only 1 incomplete quest', () => {
-  const quest1 = new QuestState(state, 'test', 'true', ['true'], ['true']);
-  const quest2 = new QuestState(state, 'test', 'false', ['false'], ['false']);
-  const quest3 = new QuestState(state, 'test', 'false', ['false'], ['false']);
-  state.questStates = [quest1, quest2, quest3];
-  expect(state.questState(0).complete()).toBe(true);
-  expect(state.questState(1).complete()).toBe(false);
-  expect(state.quests().length).toBe(2);
+  const quest1 = new QuestState(app, 'test', 'true', ['true'], ['true']);
+  const quest2 = new QuestState(app, 'test', 'false', ['false'], ['false']);
+  const quest3 = new QuestState(app, 'test', 'false', ['false'], ['false']);
+  app.questStates = [quest1, quest2, quest3];
+  expect(app.questState(0).complete()).toBe(true);
+  expect(app.questState(1).complete()).toBe(false);
+  expect(app.quests().length).toBe(2);
 });
